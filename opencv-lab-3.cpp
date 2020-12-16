4c35c03bd1dde0b55fd5da7b553fc982d6770b70
rikandart <pizhun97@gmail.com>
2020-12-16 17:12:41 +0300

    added kmeans algo

--- opencv-lab-3.cpp
+++ opencv-lab-3.cpp
@@ -9,7 +9,10 @@
 // c++
 #include <iostream>
 #include <vector>
-#include <thread>
+#include <cstdlib>
+#include <random>
+#include <algorithm>
+#include <numeric>
 #include <math.h>
 
 #define THRESHOLD 65
@@ -37,110 +40,90 @@ void show(const Mat& frame_1, const char* name_1) {
 	waitKey(0);
 }
 
-// квантование
-void editQuantizeLevel(const unsigned level, const Mat& img, Mat& dest_img) {
-	if (remainder(256.0, level) != 0.0 || level == 0 || level == 1) {
-		std::cout << "Incorrect quantize level" << std::endl;
-		return;
-	}
-	// заполнение матрицы нулями
-	dest_img = Mat::zeros(img.rows, img.cols, CV_8U);
-	// шаг квантования
-	const double step = 255.0 / (level - 1);
-	// пробегаем по пикселям x и y
-	for (unsigned i = 0; i < img.cols; i++) {
-		for (unsigned j = 0; j < img.rows; j++) {
-			unsigned br = img.at<unsigned char>(j, i);
-			for (unsigned k = 0; k < level; k++) {
-				// смотрим в каком интервале лежит число
-				if (br >= k * step && br <= (k + 1) * step / 2) {
-					// смотрим, какая разница меньше и присваиваем нужную градацию
-					dest_img.at<unsigned char>(j, i) = k * step;
-					break;
-				}
-				else if (br >= k * step / 2 && br <= (k + 1) * step) {
-					dest_img.at<unsigned char>(j, i) = (k + 1) * step;
-					break;
-				}
-			}
+// генерация случайных индексов в матрице
+void generate_rand_indices(const Mat& data, const int K, std::vector<Point>& res) {
+	std::vector<unsigned> i_res, j_res;
+	std::default_random_engine generator;
+	std::normal_distribution<double> distribution_i(0, data.rows);
+	std::normal_distribution<double> distribution_j(0, data.cols);
+	unsigned rand_i = 0, rand_j = 0;
+	bool done = false;
+	while (!done) {
+		rand_i = abs(round(distribution_i(generator)));
+		rand_j = abs(round(distribution_j(generator)));
+		if (std::find(i_res.begin(), i_res.end(), rand_i) != i_res.end()
+			&& std::find(j_res.begin(), j_res.end(), rand_j) != j_res.end()) continue;
+		else {
+			i_res.push_back(rand_i);
+			j_res.push_back(rand_j);
 		}
+		if (i_res.size() == K && j_res.size() == K) done = true;
 	}
+	for (unsigned i = 0; i < K; i++) res.push_back(Point(i_res[i], j_res[i]));
 }
 
-// гистограммы проекций
-void getHist(const Mat& image, Mat* out_hist_arr)
-{
-	// гор. гистограмма
-	out_hist_arr[0] = Mat::zeros(image.cols, image.rows, CV_8U);
-	// верт. гистограмма
-	out_hist_arr[1] = Mat::zeros(image.rows, image.cols, CV_8U);
-	for (int i = 0; i < image.rows; i++) {
-		int hist_i = -1;
-		for (int j = 0; j < image.cols; j++) {
-			if (image.at<unsigned char>(i, j) > 0) {
-				out_hist_arr[0].at<unsigned char>(image.cols-1 - ++hist_i, i) = 255;
-			}
-		}
+// заливка кластеров
+void cluster_fill(const int K, const std::vector<Point>& centroids,
+				const std::vector<std::vector<Point>>& clusters, Mat& out) {
+	// заполним матрицу-результат для отображения на экране
+	out = Mat(out.rows, out.cols, CV_32F);
+	for (unsigned k = 0; k < K; k++) {
+		unsigned color = 255 / (k + 1);
+		for (Point i : clusters[k])	out.at<float>(i.x, i.y) = (float)(color);
 	}
-		
-	for (int j = 0; j < image.cols; j++) {
-		int hist_i = -1;
-		for (int i = 0; i < image.rows; i++) {
-			if (image.at<unsigned char>(i, j) > 0) {
-				out_hist_arr[1].at<unsigned char>(image.rows-1 - ++hist_i, j) = 255;
-			}
-		}
-	}
-	for(int i = 0; i < out_hist_arr[0].cols; i++)
-		out_hist_arr[0].at<unsigned char>(out_hist_arr[0].rows-1-THRESHOLD, i) = 255;
-	for (int i = 0; i < out_hist_arr[1].cols; i++)
-		out_hist_arr[1].at<unsigned char>(out_hist_arr[1].rows - 1 - THRESHOLD_2, i) = 255;
-	bitwise_not(out_hist_arr[0], out_hist_arr[0]);
-	bitwise_not(out_hist_arr[1], out_hist_arr[1]);
+	out.convertTo(out, CV_8U);
 }
 
-// получение стробов
-void proj(const Mat& image, Mat* strobes) {
-	// x strobe
-	strobes[0] = Mat::zeros(image.rows, image.cols, CV_8U);
-	// y stobe
-	strobes[1] = Mat::zeros(image.rows, image.cols, CV_8U);
-	for (int i = 0; i < image.rows; i++) {
-		unsigned count_i = 0;
-		for (int j = 0; j < image.cols; j++) {
-			static bool write = false;
-			if (image.at<unsigned char>(i,j) > 0 && !write)
-				count_i++;
-			// сравниваем с порогом
-			if (count_i > THRESHOLD && !write) {
-				j = 0;
-				write = true;
+void kmeans(const Mat& data, const int K, Mat& out) {
+	std::vector<Point> centroids;
+	std::vector<std::vector<Point>> clusters(K);
+	std::vector<double> distances(K);
+	generate_rand_indices(data, K, centroids);
+	#define cent_i centroids[k].x
+	#define cent_j centroids[k].y
+	bool done = false;
+	auto acc_func = [](Point& p1, Point& p2)->Point {
+		return Point(p1.x+p2.x, p1.y+p2.y);
+	};
+	while (!done) {
+		// предварительная очитска кластеров
+		for (unsigned k = 0; k < K; k++) clusters[k].clear();
+		for (unsigned i = 0; i < data.rows; i++)
+			for (unsigned j = 0; j < data.rows; j++) {
+				for (unsigned k = 0; k < K; k++) {
+					// евклидово расстояние
+					double pw = pow(data.at<Vec3b>(i, j)[0] - data.at<Vec3b>(cent_i, cent_j)[0], 2);
+					std::cout << pw << std::endl;
+					pw++;
+					distances[k] = sqrt(pow(data.at<Vec3b>(i, j)[0] - data.at<Vec3b>(cent_i, cent_j)[0], 2) +
+						pow(data.at<Vec3b>(i, j)[1] - data.at<Vec3b>(cent_i, cent_j)[1], 2) +
+						pow(data.at<Vec3b>(i, j)[2] - data.at<Vec3b>(cent_i, cent_j)[2], 2));
+				}
+				// ищем минимальное расстояние = ищем индекс минимального эл-та
+				unsigned k = std::min_element(distances.begin(), distances.end()) - distances.begin();
+				std::cout << "K in cycle " << k << std::endl;
+				clusters[k].push_back(Point(i, j));
 			}
-			if (write)
-				strobes[0].at<unsigned char>(i, j) = image.at<unsigned char>(i, j);
+		unsigned done_count = 0;
+		// пересчет центроидов
+		for (unsigned k = 0; k < K; k++) {
+			Point sum = std::accumulate(clusters[k].begin(), clusters[k].end(), centroids[k], acc_func);
+			std::cout << "k " << k << " x " << sum.x << " y " << sum.y << " clust size " << clusters[k].size() << std::endl;
+			Point avg(sum.x / clusters[k].size(), sum.y / clusters[k].size());
+			if (avg.x == centroids[k].x && avg.y == centroids[k].y) done_count++;
+			else centroids[k] = avg;
 		}
+		if (done_count == K) done = true;
 	}
-	for (int j = 0; j < image.cols; j++) {
-		unsigned count_j = 0;
-		for (int i = 0; i < image.rows; i++) {
-			static bool write = false;
-			if (image.at<unsigned char>(i, j) > 0)
-				count_j++;
-			if (count_j > THRESHOLD_2 && !write) {
-				i = 0;
-				write = true;
-			}
-			if (write)
-				strobes[1].at<unsigned char>(i, j) = image.at<unsigned char>(i, j);
-		}
-	}
+	cluster_fill(K, centroids, clusters, out);
 }
 
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "Russian");
-	Mat frame_1, frame_2, outline, opened, closed;
-	Mat ones = Mat::ones(2, 2, CV_8U);
+	Mat img = imread("E:/kmeans_img.png"), res = Mat::zeros(img.rows, img. cols, CV_8U);
+	kmeans(img, 4, res);
+	show(img, res, "Original", "Clustered");
 	return 0;
 }
 #endif
