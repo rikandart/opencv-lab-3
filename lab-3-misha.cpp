5548bdb298b20024f989b5bdc8e8be9ec087cc5c
rikandart <pizhun97@gmail.com>
2020-12-16 21:53:28 +0300

    my_end misha_end

--- lab-3-misha.cpp
+++ lab-3-misha.cpp
@@ -1,41 +1,99 @@
-//#define MISHA
+#define MISHA
 #ifdef MISHA
+// opencv lib
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+
+// c++
 #include <iostream>
-#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include <math.h>
 
 using namespace cv;
 
-void main() {
-	Mat img1, img2;
-	img1 = imread("C:\\vlcsnap-2020-11-25-10h10m50s000.png");
-	img2 = imread("C:\\vlcsnap-2020-11-25-10h10m42s459.png");
-	Mat razn;
-	// вычисление разностного изображения
-	absdiff(img1, img2, razn);
-	imshow("Разница", razn);
-	Mat canny, thresh, ba;
-	// применение маски кенни
-	Canny(img2, canny, 50, 200);
-	imshow("Матрица Кенни", canny);
-	std::cout << canny.rows << " " << canny.cols << std::endl;
-	std::cout << razn.rows << " " << razn.cols << std::endl;
-	Mat razn2, canny2;
-	cvtColor(razn, razn2, COLOR_RGB2GRAY);
-	
-	bitwise_and(canny, razn2, ba);
-	imshow("Результат И", ba);
-	threshold(ba, ba, 5, 255, THRESH_BINARY_INV);
-	imshow("Пороговая бинаризация", ba);
-	Mat element = getStructuringElement(MORPH_RECT, Size(6, 6), Point(3, 3));
-	morphologyEx(ba, ba, MORPH_ERODE, element);
-	imshow("Морфологическая фильтрация", ba);
-
+int main(int argc, char* argv[])
+{
+	Mat pic = imread("E:/kmeans_img.png");
+	Mat edit = Mat(pic.rows, pic.cols, CV_32F);
+	const int K = 5;
+	//нечет ряды, чет колонки
+	int centr_xy[K*2];
+	int rasst[K];
+	for (int i = 0; i < K; i++) {
+		centr_xy[i*2] = rand() % pic.rows;
+		centr_xy[i*2+1] = rand() % pic.cols;
+	}
+	//ргб центров
+	double centr_rgb[K * 3];
+	for (int i = 0; i < K; i++) {
+		centr_rgb[i * 3] = pic.at<Vec3b>(centr_xy[i * 2], centr_xy[i * 2 + 1])[0];
+		centr_rgb[i * 3 + 1] = pic.at<Vec3b>(centr_xy[i * 2], centr_xy[i * 2 + 1])[1];
+		centr_rgb[i * 3 + 2] = pic.at<Vec3b>(centr_xy[i * 2], centr_xy[i * 2 + 1])[2];
+	}
+	int mult = pic.rows * pic.cols;
+	Mat labels = Mat::zeros(1, mult, CV_8U);
+	for (int a = 0; a < 10; a++) {
+		for (int i = 0; i < pic.rows; i++)
+			for (int j = 0; j < pic.cols; j++) {
+				for (int k = 0; k < K; k++) {
+					double r = pic.at<Vec3b>(i, j)[0] - centr_rgb[k * 3];
+					double g = pic.at<Vec3b>(i, j)[1] - centr_rgb[k * 3 + 1];
+					double b = pic.at<Vec3b>(i, j)[2] - centr_rgb[k * 3 + 2];
+					rasst[k] = sqrt(r * r + g * g + b * b);
+				}
+				int b = 0;
+				int min = rasst[0];
+				for (int k = 0; k < K; k++) {
+					if (rasst[k] < min) {
+						min = rasst[k];
+						b = k;
+					}
+				}
+				labels.at<unsigned char>(0, i * pic.cols + j) = b;
+			}
+		int sum[K * 3];
+		int n[K];
+		//сумма по всему кластеру
+		for (int b = 0; b < labels.cols; b++)
+			for(int k = 0; k < K; k++)
+				if (labels.at<unsigned char>(0, b) == k) {
+					sum[k * 3] += pic.at<Vec3b>(b / pic.cols, b % pic.cols)[0];
+					sum[k * 3 + 1] += pic.at<Vec3b>(b / pic.cols, b % pic.cols)[1];
+					sum[k * 3 + 2] += pic.at<Vec3b>(b / pic.cols, b % pic.cols)[2];
+					n[k]++;
+				}
+		//центры
+		int povt = 0;
+		for (int k = 0; k < K; k++) {
+			int tmpr = centr_rgb[k * 3], tmpg = centr_rgb[k * 3 + 1],
+				tmpb = centr_rgb[k * 3 + 2];
+			if (n[k] != 0) {
+				centr_rgb[k * 3] = ((double)sum[k * 3]) / n[k];
+				centr_rgb[k * 3 + 1] = ((double)sum[k * 3 + 1]) / n[k];
+				centr_rgb[k * 3 + 2] = ((double)sum[k * 3 + 2]) / n[k];
+				if (centr_rgb[k * 3] == tmpr && centr_rgb[k * 3 + 1] == tmpg &&
+					centr_rgb[k * 3 + 2] == tmpb) povt++;
+			}
+		}
+		if (povt == K) break;
+	}
+	//заполнение
+	int colors[K];
+	for (int i = 0; i < K; i++) {
+		colors[i] = 255 / (i + 1);
+	}
+	for (int i = 0; i < mult; i++) {
+		edit.at<float>(i / pic.cols, i % pic.cols) =
+			(float)(colors[labels.at<unsigned char>(0, i)]);
+	}
+	edit.convertTo(edit, CV_8U);
+	imshow("Pic", pic);
+	imshow("Edit", edit);
 	waitKey(0);
-
+	return 0;
 }
-
 #endif
+
--- opencv-lab-3.cpp
+++ opencv-lab-3.cpp
@@ -1,4 +1,4 @@
-//#define MISHA
+#define MISHA
 #ifndef MISHA
 // opencv lib
 #include <opencv2/core.hpp>
@@ -39,16 +39,18 @@ void show(const Mat& frame_1, const char* name_1) {
 }
 
 // генерация случайных индексов в матрице
-void generate_rand_indices(const Mat& data, const unsigned K, std::vector<Point>& res) {
+void generate_rand_rgb(const Mat& data, const unsigned K, std::vector<Vec3b>& res) {
 	std::vector<unsigned> i_res, j_res;
 	std::default_random_engine generator;
-	std::normal_distribution<double> distribution_i(0, data.rows/1.5);
-	std::normal_distribution<double> distribution_j(0, data.cols/1.5);
+	std::normal_distribution<double> distribution_i(0, data.rows/3);
+	std::normal_distribution<double> distribution_j(0, data.cols/3);
 	unsigned rand_i = 0, rand_j = 0;
 	bool done = false;
 	while (!done) {
 		rand_i = abs(round(distribution_i(generator)));
 		rand_j = abs(round(distribution_j(generator)));
+		if(rand_i > data.rows) rand_i = data.rows;
+		if(rand_j > data.cols) rand_j = data.cols;
 		if (std::find(i_res.begin(), i_res.end(), rand_i) != i_res.end()
 			&& std::find(j_res.begin(), j_res.end(), rand_j) != j_res.end()) continue;
 		else {
@@ -57,12 +59,11 @@ void generate_rand_indices(const Mat& data, const unsigned K, std::vector<Point>
 		}
 		if (i_res.size() == K && j_res.size() == K) done = true;
 	}
-	for (unsigned i = 0; i < K; i++) res.push_back(Point(i_res[i], j_res[i]));
+	for (unsigned i = 0; i < K; i++) res.push_back(Vec3b(data.at<Vec3b>(i_res[i], j_res[i])));
 }
 
 // заливка кластеров
-void cluster_fill(const unsigned K, const std::vector<Point>& centroids,
-				const std::vector<std::vector<Point>>& clusters, Mat& out) {
+void cluster_fill(const unsigned K, const std::vector<std::vector<Point>>& clusters, Mat& out) {
 	// заполним матрицу-результат для отображения на экране
 	out = Mat(out.rows, out.cols, CV_32F);
 	for (unsigned k = 0; k < K; k++) {
@@ -72,40 +73,47 @@ void cluster_fill(const unsigned K, const std::vector<Point>& centroids,
 	out.convertTo(out, CV_8U);
 }
 
-void kmeans(const Mat& data, const unsigned K, Mat& out, const unsigned iter = 10) {
-	std::vector<Point> centroids;
+void kmeans(const Mat& data, const unsigned K, Mat& out, unsigned& it, const unsigned max_iter = 10) {
+	std::vector<Vec3b> centroids;
 	std::vector<std::vector<Point>> clusters(K);
 	std::vector<double> distances(K);
-	generate_rand_indices(data, K, centroids);
+	generate_rand_rgb(data, K, centroids);
 	bool done = false;
-	auto acc_func = [](Point& p1, Point& p2)->Point {
-		return Point(p1.x+p2.x, p1.y+p2.y);
-	};
-	for (unsigned it = 0; it < iter; it++) {
+	it = 0;
+	// ждем пока все центройды повторятся, либо пройдет максимальное кол-во итераций
+	while(!done && it != max_iter) {
 		// предварительная очитска кластеров
 		for (unsigned k = 0; k < K; k++) clusters[k].clear();
 		for (unsigned i = 0; i < data.rows; i++)
 			for (unsigned j = 0; j < data.cols; j++) {
 				for (unsigned k = 0; k < K; k++) {
 					// евклидово расстояние
-					distances[k] = sqrt(pow(data.at<Vec3b>(i, j)[0] - data.at<Vec3b>(centroids[k].x, centroids[k].y)[0], 2) +
-						pow(data.at<Vec3b>(i, j)[1] - data.at<Vec3b>(centroids[k].x, centroids[k].y)[1], 2) +
-						pow(data.at<Vec3b>(i, j)[2] - data.at<Vec3b>(centroids[k].x, centroids[k].y)[2], 2));
+					distances[k] = sqrt(pow(data.at<Vec3b>(i, j)[0] - centroids[k][0], 2) +
+						pow(data.at<Vec3b>(i, j)[1] - centroids[k][1], 2) +
+						pow(data.at<Vec3b>(i, j)[2] - centroids[k][2], 2));
 				}
 				// ищем минимальное расстояние = ищем индекс минимального эл-та
 				unsigned k = std::min_element(distances.begin(), distances.end()) - distances.begin();
 				clusters[k].push_back(Point(i, j));
 			}
 		// пересчет центроидов
+		unsigned done_count = 0;
 		for (unsigned k = 0; k < K; k++) {
-			if (clusters[k].size()) {
-				Point sum = std::accumulate(clusters[k].begin(), clusters[k].end(), centroids[k], acc_func);
-				Point avg(sum.x / clusters[k].size(), sum.y / clusters[k].size());
-				centroids[k] = avg;
+			unsigned cl_size = clusters[k].size();
+			if (cl_size) {
+				Vec3b tmp(centroids[k]);
+				Vec3d sum(0, 0, 0);
+				for (Point i : clusters[k]) sum += data.at<Vec3b>(i.x, i.y);
+				// среднее по всем каналам
+				centroids[k] = Vec3b(sum[0] / cl_size, sum[1] / cl_size, sum[2] / cl_size);
+				if (centroids[k] == tmp) done_count++;
 			}
 		}
+		// если все центроиды повторились, заканчиваем расчет
+		if (done_count == K) done = true;
+		it++;
 	}
-	cluster_fill(K, centroids, clusters, out);
+	cluster_fill(K, clusters, out);
 }
 
 int main(int argc, char* argv[])
@@ -115,16 +123,21 @@ int main(int argc, char* argv[])
 	auto show_lam = [&]() {show(img, res, "Original", "Clustered");};
 	while (true) {
 		std::cout << "Введите кол-во кластеров, -1 для выхода > ";
-		int k = 0, iter = 0;
+		int k = 0, max_iter = 0;
 		std::cin >> k;
 		std::cout << std::endl;
 		if (k == -1) break;
-		std::cout << "Введите кол-во итераций > ";
-		std::cin >> iter;
+		std::cout << "Введите максимальное кол-во итераций > ";
+		std::cin >> max_iter;
 		std::cout << std::endl;
-		if (k * iter <= 0) std::cout << "Данные введены неверно" << std::endl;
-		kmeans(img, k, res, iter);
-		std::thread(show_lam).detach();
+		if (k * max_iter <= 0) std::cout << "Данные введены неверно" << std::endl;
+		else {
+			unsigned it = 0;
+			kmeans(img, k, res, it, max_iter);
+			std::thread(show_lam).detach();
+			std::cout << "Прошло итераций: " << it << std::endl;
+		}
+		system("pause");
 		system("cls");
 	}
 	return 0;
